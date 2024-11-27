e7788d1a34638c369715056a174d13973d1c4f87
Furkan Yılmaz <frknylmz1566@gmail.com>
2024-11-28 00:22:37 +0300

    first commit
    project inject

---
+++ src/views/MainPage.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="flex items-center justify-center h-screen bg-black">
+    <div class=" p-4 rounded-md shadow-lg bg-black">
+      <div class="mb-5">
+          <p class="text-blue-600  text-2xl">FURKAN YILMAZ</p>
+      </div>
+      <div class="flex space-x-4 ml-5">      
+        <!-- LinkedIn Button -->
+        <a 
+          href="https://www.linkedin.com/in/furkanyilmaz1566/" 
+          target="_blank" 
+          class="flex items-center justify-center bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-600 transition"
+        >
+          <img 
+            src="https://upload.wikimedia.org/wikipedia/commons/c/ca/LinkedIn_logo_initials.png" 
+            alt="LinkedIn Logo" 
+            class="w-6 h-6"
+          />
+        </a>
+        <!-- GitHub Button -->
+        <a 
+          href="https://github.com/" 
+          target="_blank" 
+          class="flex items-center justify-center bg-gray-800 text-white py-2 px-4 rounded hover:bg-gray-900 transition"
+        >
+          <img 
+            src="https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png" 
+            alt="GitHub Logo" 
+            class="w-6 h-6 "
+          />
+         
+        </a>
+      </div>
+    </div>
+  </div>
+</template>
+
+
+<script setup>
+
+</script>
